b8cf965588da6fbd5338f5545120b3a09e67aa6e
Gaponec <uaGaponec@gmail.com>
2016-07-24 20:20:53 +0300

    Initial commit

---
+++ test/index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gaponec
+ * Date: 7/24/16
+ * Time: 6:41 PM
+ */
+
+const FAULT_URL = 'https://nauguide.esy.es/';
+const DEFAULT_TOKEN = 'MqL0c8tKCtheLSYcygYNtGhU8Z2hULOFs9OKPdEp';
+const DEFAULT_PATH = '/api-firebase/test';
+
+$firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
+
+// --- storing an array ---
+$test = array(
+    "foo" => "bar",
+    "i_love" => "lamp",
+    "id" => 42
+);
+$dateTime = new DateTime();
+$firebase->set(DEFAULT_PATH . '/' . $dateTime->format('c'), $test);
+
+// --- storing a string ---
+$firebase->set(DEFAULT_PATH . '/name/contact001', "John Doe");
+
+// --- reading the stored string ---
+$name = $firebase->get(DEFAULT_PATH . '/name/contact001');
+
+?>
